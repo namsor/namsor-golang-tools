18e030c845fb1e9d4384e60ecc088f2aa2964f53
Oshan96 <oshanmen@gmail.com>
2020-11-29 22:31:22 +0530

    structure and variable setup

--- src/com/namsor/tools/NamSorTools.go
+++ src/com/namsor/tools/NamSorTools.go
@@ -1,8 +1,104 @@
 package main
 
 import (
+	"crypto"
 	"flag"
+	namsorapi "github.com/namsor/namsor-golang-sdk2"
+	"hash"
 )
+
+const DEFAULT_DIGEST_ALGO string = "MD5"
+const BATCH_SIZE int = 100
+
+const INPUT_DATA_FORMAT_FNLN string = "fnln"
+const INPUT_DATA_FORMAT_FNLNGEO string = "fnlngeo"
+const INPUT_DATA_FORMAT_FULLNAME string = "name"
+const INPUT_DATA_FORMAT_FULLNAMEGEO string = "namegeo"
+
+var INPUT_DATA_FORMAT = [4]string{
+	INPUT_DATA_FORMAT_FNLN,
+	INPUT_DATA_FORMAT_FNLNGEO,
+	INPUT_DATA_FORMAT_FULLNAME,
+	INPUT_DATA_FORMAT_FULLNAMEGEO,
+}
+
+var INPUT_DATA_FORMAT_HEADER = [4][]string{
+	{"firstName", "lastName"},
+	{"firstName", "lastName", "countryIso2"},
+	{"fullName"},
+	{"fullName", "countryIso2"},
+}
+
+const SERVICE_NAME_PARSE string = "parse"
+const SERVICE_NAME_GENDER string = "gender"
+const SERVICE_NAME_ORIGIN string = "origin"
+const SERVICE_NAME_COUNTRY string = "country"
+const SERVICE_NAME_DIASPORA string = "diaspora"
+const SERVICE_NAME_USRACEETHNICITY string = "usraceethnicity"
+
+var SERVICES = []string{
+	SERVICE_NAME_PARSE,
+	SERVICE_NAME_GENDER,
+	SERVICE_NAME_ORIGIN,
+	SERVICE_NAME_COUNTRY,
+	SERVICE_NAME_DIASPORA,
+	SERVICE_NAME_USRACEETHNICITY,
+}
+
+var OUTPUT_DATA_PARSE_HEADER = []string{
+	"firstNameParsed",
+	"lastNameParsed",
+	"nameParserType",
+	"nameParserTypeAlt",
+	"nameParserTypeScore",
+	"script",
+}
+var OUTPUT_DATA_GENDER_HEADER = []string{
+	"likelyGender",
+	"likelyGenderScore",
+	"probabilityCalibrated",
+	"genderScale",
+	"script",
+}
+
+var OUTPUT_DATA_ORIGIN_HEADER = []string{
+	"countryOrigin",
+	"countryOriginAlt",
+	"probabilityCalibrated",
+	"probabilityCalibratedAlt",
+	"countryOriginScore",
+	"script",
+}
+var OUTPUT_DATA_COUNTRY_HEADER = []string{
+	"country",
+	"countryAlt",
+	"probabilityCalibrated",
+	"probabilityCalibratedAlt",
+	"countryScore",
+	"script",
+}
+var OUTPUT_DATA_DIASPORA_HEADER = []string{
+	"ethnicity",
+	"ethnicityAlt",
+	"ethnicityScore",
+	"script",
+}
+var OUTPUT_DATA_USRACEETHNICITY_HEADER = []string{
+	"raceEthnicity",
+	"raceEthnicityAlt",
+	"probabilityCalibrated",
+	"probabilityCalibratedAlt",
+	"raceEthnicityScore",
+	"script",
+}
+var OUTPUT_DATA_HEADERS = [][]string{
+	OUTPUT_DATA_PARSE_HEADER,
+	OUTPUT_DATA_GENDER_HEADER,
+	OUTPUT_DATA_ORIGIN_HEADER,
+	OUTPUT_DATA_COUNTRY_HEADER,
+	OUTPUT_DATA_DIASPORA_HEADER,
+	OUTPUT_DATA_USRACEETHNICITY_HEADER,
+}
 
 var (
 	apiKey          string
@@ -18,6 +114,32 @@ var (
 	endpoint        string
 	encoding        string
 )
+
+type NamrSorTools struct {
+	done         []string
+	separatorOut string
+	separatorIn  string
+	personalApi  *namsorapi.PersonalApiService
+	adminApi     *namsorapi.AdminApiService
+	TIMEOUT      int
+	withUID      bool
+	recover      bool
+	skipErrors   bool
+	digest       hash.Hash
+}
+
+func New(tools NamrSorTools) NamrSorTools {
+	config := namsorapi.NewConfiguration()
+	client := namsorapi.NewAPIClient(config)
+	tools.separatorIn = "|"
+	tools.separatorOut = "|"
+	tools.adminApi = client.AdminApi
+	tools.personalApi = client.PersonalApi
+	tools.TIMEOUT = 30000
+	tools.digest = crypto.MD5.New()
+
+	return tools
+}
 
 func main() {
 	flag.StringVar(&apiKey, "apiKey", "", "NamSor API Key")
